3d32eed139eeb57eb12b91d6513496db75c18424
Tyler Pires <93809267+Tylercpires@users.noreply.github.com>
2025-03-08 14:34:35 -0800

    Initial Data Parsing
    Added parsing to dataset to acquire feature matrix and target/label vector

--- knn.py
+++ knn.py
@@ -4,20 +4,41 @@
 
 #Dependencies
 import kagglehub #pip install kagglehub
+import pandas as pd #pip install pandas
 
 #Control Switches
-debug = 1 #Switch for debugging
+debug = 0 #Switch for debugging
 
 def main():
+
+    print("\nStarted execution of knn.py...\n")
 
     #https://www.kaggle.com/datasets/amitanshjoshi/spotify-1million-tracks
     #Ensure the latest version of the dataset is downloaded.
     path = kagglehub.dataset_download("amitanshjoshi/spotify-1million-tracks") #If this is failing for some reason, look in :
                                                                                #C:\Users\(insert user name here)\.cache\kagglehub\datasets\amitanshjoshi\spotify-1million-tracks\versions\1
+    
+    #Ensure path points to the CSV file instead of the directory
+    path += r"\spotify_data.csv"
+
     if debug == 1:
         print("Path to dataset files:", path)
+        
+    print("Dataset updated.")
 
-    print("Hello world")
+    data = pd.read_csv(path)
+    
+    #Split data into feature matrix and target/label vector
+    x = data.drop(columns=["Unnamed: 0", "artist_name", "track_name", "track_id", "genre"]) #"Unnamed: 0" is the enumeration column
+    y = data["genre"]
+    
+    if debug == 1:
+        print(x.columns.tolist())
+        print(y)
+
+    print("Dataset split into feature matrix and target/label vector.")
+    
+    print("\nFinished execution of knn.py.")
     
 if __name__ == "__main__":
     main()
