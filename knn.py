c8a9b21e2fe37de9dd6a267125ca83fb0e6f19af
Tyler Pires <93809267+Tylercpires@users.noreply.github.com>
2025-03-08 13:42:23 -0800

    Add initial file

---
+++ knn.py
@@ -0,0 +1,11 @@
+#T. Pires - knn.py - K-NN python script for SENG474 project.
+#Created 2025-03-08
+#Last Updated 2025-03-08
+
+#Note: Attempting to load dataset using import as opposed to download.
+
+def main():
+    print("Hello world")
+    
+if __name__ == "__main__":
+    main()
